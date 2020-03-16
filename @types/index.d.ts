fcdd80bd9f2039244458cce04c8d26f2f2d45677
Joe Summerhayes <joesummerhayes09@hotmail.com>
2020-03-16 10:53:16 +0000

    add namespcae and made avilable to front and backend

---
+++ @types/index.d.ts
@@ -0,0 +1,8 @@
+import { PremTeam as IPremTeam } from './prem-team';
+
+declare namespace FFType {
+  export type PremTeam = IPremTeam;
+}
+
+export = FFType;
+export as namespace FFType;
---
+++ @types/prem-team.d.ts
@@ -0,0 +1,8 @@
+
+export interface PremTeam {
+  _id: string;
+  cleanName: string;
+  id: string;
+  image: string;
+  name: string;
+}
