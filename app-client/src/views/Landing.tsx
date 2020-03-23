50ff7a81c121f046cda7246a47466a163b06a47b
Joe Summerhayes <joesummerhayes09@hotmail.com>
2020-03-23 14:51:57 +0000

    Basic auth0set up and front end implimentation

--- app-client/src/views/Landing.tsx
+++ app-client/src/views/Landing.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import getPremTeam from '../data/prem-team';
+import { Auth0Context } from '../contexts/auth0-context';
 
-const Landing: React.FC = () => {
+
+const Landing = () => {
   const foo = getPremTeam('5e6c0c6902654f24f473cd74');
+
+  const { isLoading, user, loginWithRedirect, logout } = useContext(Auth0Context);
+
   console.log(foo);
   return (
-    <div>
-      This it the landing page
+    <div className="hero is-info is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          {!isLoading && !user && (
+            <>
+              <h1>Click Below!</h1>
+              <button type="button" onClick={loginWithRedirect} className="button is-danger">
+                Login
+              </button>
+            </>
+          )}
+          {!isLoading && user && (
+            <>
+              <h1>You are logged in!</h1>
+              <p>Hello {user.name}</p>
+
+              {user.picture && <img src={user.picture} alt="My Avatar" />}
+              <button
+                type="button"
+                onClick={() => logout({ returnTo: window.location.origin })}
+                className="button is-small is-dark"
+              >
+                Logout
+              </button>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
