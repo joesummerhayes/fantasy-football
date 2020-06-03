48d3077a4dede67f7188e0a4ef0f644416f92fe5
Joe Summerhayes <joesummerhayes09@hotmail.com>
2020-06-03 16:42:58 +0100

    make login query

---
+++ app-client/src/data/login.ts
@@ -0,0 +1,34 @@
+import graphQL from './graph-ql';
+
+interface LoggedInUser {
+  token: string;
+  userId: string;
+}
+
+interface UserQLResult {
+  loggedInUser: LoggedInUser;
+}
+
+interface LoginVariables {
+  email: string;
+  password: string;
+}
+
+const login = async (variables: any): Promise<LoggedInUser> => {
+  const response = await graphQL.query<UserQLResult>(`
+    query loggingInuser($email: String!, $password: String!){
+      login(email: $email, password: $password) {
+        token
+        userId
+      }
+    }`,
+  variables);
+  console.log('2222');
+  if (response === null) {
+    throw new Error('There was a problem loggin in user');
+  }
+  console.log('3333', response);
+  return response.loggedInUser;
+};
+
+export default login;
