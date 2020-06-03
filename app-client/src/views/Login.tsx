8a337fa5c37a4c3aec287430b6c9f7b7de8d43b0
Joe Summerhayes <joesummerhayes09@hotmail.com>
2020-06-03 16:42:34 +0100

    login page and form logic

--- app-client/src/views/Login.tsx
+++ app-client/src/views/Login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import { Box } from '@material-ui/core';
+import login from '../data/login';
 
 const useStyles = makeStyles({
   root: {
@@ -17,19 +18,60 @@ const useStyles = makeStyles({
 });
 
 const Login: React.FC = () => {
+  const [form, setForm] = React.useState({
+    email: '',
+    password: '',
+  });
   const classes = useStyles();
+
+  const testing = (e: any) => {
+    e.preventDefault();
+    login(form);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { target } = event;
+    const { value } = target;
+    const { id } = target;
+
+    const updatedForm = {
+      ...form,
+      [id]: value,
+    };
+
+    setForm(updatedForm);
+  };
+
+  console.log(form);
+
   return (
     <Box display="flex">
-      <form className={classes.root}>
+      <form className={classes.root} onSubmit={testing}>
         <div>
           Login
         </div>
         <div className={classes.inputField}>
-          <TextField fullWidth variant="outlined">Hello</TextField>
+          <TextField
+            fullWidth
+            variant="outlined"
+            value={form.email}
+            placeholder="email"
+            onChange={handleInputChange}
+            id="email"
+          />
         </div>
         <div className={classes.inputField}>
-          <TextField fullWidth variant="outlined">Hello</TextField>
+          <TextField
+            fullWidth
+            variant="outlined"
+            value={form.password}
+            placeholder="password"
+            onChange={handleInputChange}
+            id="password"
+            type="password"
+          />
         </div>
+        <button type="submit">hello</button>
       </form>
     </Box>
   );
