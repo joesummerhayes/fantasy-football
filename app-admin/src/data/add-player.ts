f9247497c6eb768a49377facdffd9991236d8359
Joe Summerhayes <joesummerhayes09@hotmail.com>
2020-06-21 17:41:11 +0100

    graphql data request

---
+++ app-admin/src/data/add-player.ts
@@ -0,0 +1,25 @@
+import graphQL from './graph-ql';
+
+interface PlayerQlResult {
+  player: FFType.Player;
+}
+
+const addPlayer = async (variables: any): Promise<FFType.Player> => {
+  const response = await graphQL.query<PlayerQlResult>(`
+    mutation: addNewPlayer($firstName: String!, $lastName: String!, $position: String!, $team: String!) {
+      addPlayer(playerInput: {firstName: $firstName, lastName: $lastName, position: $position, team: $team}) {
+        _id
+        firstName
+        lastName
+        position
+        team
+      }
+    }
+  `, variables);
+  if (response === null) {
+    throw new Error('There was a problem adding player');
+  }
+  return response.player;
+};
+
+export default addPlayer;
