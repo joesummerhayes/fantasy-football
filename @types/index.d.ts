4aaa5ffbde8ca8304c743c7c8c09d887edc37e8d
Joe Summerhayes <joesummerhayes09@hotmail.com>
2020-06-21 17:04:10 +0100

    make graphql resolver

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -2,6 +2,7 @@ import { PremTeam as IPremTeam } from './prem-team';
 import { User as IUser, LoggedInUser as ILoggedInUser, LoginCredentials as ILoginCredentials, SignupUser as ISignupUser } from './user';
 import { MongoUser as IMongoUser } from './mongo-user';
 import { Validator as IValidator, FormItem as IFormItem } from './form';
+import { Player as IPlayer } from './player';
 
 declare namespace FFType {
   export type PremTeam = IPremTeam;
@@ -12,6 +13,7 @@ declare namespace FFType {
   export type SignupUser = ISignupUser;
   export type Validator = IValidator;
   export type FormItem = IFormItem;
+  export type Player = IPlayer;
 }
 
 export = FFType;
