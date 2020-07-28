af3b82139bc2d8cf76f7c6776fc25c8dcacc7b36
Joe Summerhayes <joesummerhayes09@hotmail.com>
2020-07-28 18:10:58 +0100

    create league model

---
+++ api/src/models/league.ts
@@ -0,0 +1,31 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface League extends Document {
+  draftDate: Date;
+  gameweekStart: string;
+  leagueName: string;
+  members: FFType.User[];
+}
+
+const leagueSchema: Schema = new Schema({
+  draftDate: {
+    type: Date,
+    required: true,
+  },
+  gameweekStart: {
+    type: String,
+    required: true,
+  },
+  leagueName: {
+    type: String,
+    required: true,
+  },
+  members: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+export default mongoose.model<League>('League', leagueSchema);
