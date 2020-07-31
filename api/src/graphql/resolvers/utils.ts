8aa41413d6f4e0714f0a8f7e33647e5168779c65
Joe Summerhayes <joesummerhayes09@hotmail.com>
2020-07-31 17:34:56 +0100

    random passord generator

--- api/src/graphql/resolvers/utils.ts
+++ api/src/graphql/resolvers/utils.ts
@@ -7,4 +7,13 @@ export const checkFieldsExist = (values: string[]): boolean => {
   return errors.length <= 0;
 };
 
+export const generatePassword = (length: number): string => {
+  const chars = 'abcdefghijklmnopqrstuvqxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+  let password = '';
+  for (let i = 0; i < length; i += 1) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return password;
+};
+
 export default {};
