02b786eb4ed3efd1874a04cbcd74b4273b653526
Joe Summerhayes <joesummerhayes09@hotmail.com>
2020-08-04 13:16:37 +0100

    league views wip

--- app-client/src/views/leagues/index.tsx
+++ app-client/src/views/leagues/index.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 import CreateLeague from './create-league';
+import JoinLeague from './join-league';
+import { AppState } from '../../app-state';
+import withLoading from '../HOC/with-loading';
+import Loading from '../components/Loading';
+import LeagueDetails from './league-details';
+import Center from '../components/Center';
+import Button from '../components/Button';
 
-const Leagues = (): JSX.Element => {
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+  },
+  button: {
+    margin: '2rem 1rem 2rem 0',
+  },
+}));
+
+const Leagues = (): JSX.Element | null => {
+  const classes = useStyles();
+  const league = useSelector((state: AppState) => state?.user?.userDetails?.league);
+
+  if (league === undefined) {
+    return <Loading />;
+  }
+
+  const joinLeagueClick = () => {
+    console.log('direct to join league page');
+  }
+
+  const createLeagueClick = () => {
+    console.log('direct to create league page');
+  }
+
   return (
-    <CreateLeague />
+    <div className={classes.root}>
+      <Button text="Join League" clickHandler={joinLeagueClick} className={classes.button} />
+      <Button text="Create League" clickHandler={createLeagueClick} className={classes.button} />
+      { league && <Center><LeagueDetails /></Center> }
+      { !league && <CreateLeague /> }
+    </div>
   );
 };
 
---
+++ app-client/src/views/leagues/join-league.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JoinLeague: React.FC = (): JSX.Element => {
+  return (
+    <div>join a league</div>
+  );
+};
+
+export default JoinLeague;
